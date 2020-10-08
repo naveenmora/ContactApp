efd0705c3912f24873aaecd6175ffc2086980660
Naveen Mora <moranaveen1@gmail.com>
2020-10-08 23:25:15 +0530

    Added Error msg based locale

--- test/models/contact_test.rb
+++ test/models/contact_test.rb
@@ -3,17 +3,12 @@ require 'test_helper'
 class ContactTest < ActiveSupport::TestCase
 
   test "should not save contact without required_fields" do
-	  article = Contact.new
-	  assert_not article.save
+	  contact = Contact.new
+	  assert_not contact.save
 	end
 
   test "is valid with no phone number for a contact " do
-    contact =  Contact.new(
-    							first_name: 'test_first_name',
-    							last_name: 'test_last_name',
-    				      message: 'sample text',
-                  :email => 'test@domain.com',
-                 )
+    contact =  Contact.new(	first_name: 'test_first_name',last_name: 'test_last_name', message: 'sample text',:email => 'test@domain.com')
     assert contact.phone.nil?
   end
 end
